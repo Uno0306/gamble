294bafcf27dc71ada3c855348e113933a11eb703
Uno0306 <zion6878@naver.com>
2021-05-26 15:58:23 +0900

    5st failed

--- TimerTrigger1/run.py
+++ TimerTrigger1/run.py
@@ -1,8 +1,35 @@
 import os
 from bs4 import BeautifulSoup
 import urllib.request as req
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import pyrebase
 
 
+# Use a service account
+# cred = credentials.Certificate('path/to/serviceAccount.json')
+# firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
+
+# python으로 정보 넣기
+firebaseConfig = {
+    "apiKey": "AIzaSyDkHR3J8gg7ER7rUSm73nJgGN1vIKDdJLI",
+    "authDomain": "assign-gamble-web-cloud.firebaseapp.com",
+    "projectId": "assign-gamble-web-cloud",
+    "databaseURL": "https://assign-gamble-web-cloud.firebaseio.com",
+    "storageBucket": "assign-gamble-web-cloud.appspot.com",
+    "messagingSenderId": "285912167841",
+    "appId": "1:285912167841:web:9f0713758fae33b672c7e0",
+    "measurementId": "G-274ERT67VV"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+
+# 네이버에서 환율 가져오기
 url = "https://finance.naver.com/marketindex/"
 
 res = req.urlopen(url)
@@ -14,10 +41,19 @@ change = soup.select('span.change')
 blind = soup.select('div.head_info > span.blind')
 
 
-i = 0
+# i = 0
+# for c_list in soup:
+#      try:
+#           print(i+1,name_nation[i].text, name_price[i].text, change[i].text, blind[i].text)
+#           i = i + 1
+#      except IndexError:
+#           pass
+
+
+i = 2
 for c_list in soup:
      try:
-          print(i+1,name_nation[i].text, name_price[i].text, change[i].text, blind[i].text)
+          db.child("admin").child("usd").update({"2":"1202"})
           i = i + 1
      except IndexError:
           pass
---
+++ fb/conn.js
@@ -0,0 +1,16 @@
+src="https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js"
+src="https://www.gstatic.com/firebasejs/8.6.2/firebase-analytics.js"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDkHR3J8gg7ER7rUSm73nJgGN1vIKDdJLI",
+    authDomain: "assign-gamble-web-cloud.firebaseapp.com",
+    projectId: "assign-gamble-web-cloud",
+    storageBucket: "assign-gamble-web-cloud.appspot.com",
+    messagingSenderId: "285912167841",
+    appId: "1:285912167841:web:9f0713758fae33b672c7e0",
+    measurementId: "G-274ERT67VV"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
