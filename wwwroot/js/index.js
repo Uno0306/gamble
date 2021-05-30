5f4eecc5446308d8e3d9aac86116865ab8210c53
Uno0306 <zion6878@naver.com>
2021-05-31 06:29:13 +0900

    24thDeploy

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,3 +1,41 @@
 function goJoin(){
     window.location.href="/Home/join";
 }
+
+function login(){
+    var idCheck = [];
+    var userId = document.getElementById('id').value;
+    var pwdCheck = [];
+    var userPwd = document.getElementById('pwd').value;
+
+
+    $(document).ready(function(){
+        if(userId != "" && userId != undefined){
+            if(userPwd != "" && userPwd != undefined){
+                database.ref('/user/' + userId).once('value').then(function (snapshot) {
+                    snapshot.forEach(function (idSnapshot) {
+                        var idKey = idSnapshot.key;
+                        var pwdVal = idSnapshot.val();
+                        idCheck.push(idKey);
+                        pwdCheck.push(pwdVal);
+                    });
+                    if(userPwd == pwdCheck[8]){
+                        alert("로그인 성공!");
+                        window.location.href="/Home/main";
+                    }
+                    else{
+                        alert("아이디 또는 비밀번호가 잘못 되었습니다.");
+                    }
+                });
+            }else{
+                alert("비밀번호를 입력해주세요.");
+            }
+        }
+        else{
+            alert("아이디를 입력해주세요.");
+        }
+    });    
+
+   
+
+}
